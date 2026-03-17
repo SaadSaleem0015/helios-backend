57289354980b9fd505190917d7c206e4574d8e4e
Saad Naeem <jsaad3663@gmail.com>
2026-03-18 02:19:11 +0500

    --

--- controllers/admin_controller.py
+++ controllers/admin_controller.py
@@ -53,7 +53,7 @@ async def check_balance(user_id: int) -> bool:
 
 @admin_router.get("/users")
 async def get_logs(admin: Annotated[User, Depends(get_admin)]):
-    users = await User.filter(type = "user").all()
+    users = await User.filter(type = "user").all().first("-id")
 
     result = []
     for user in users:
